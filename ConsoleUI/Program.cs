26e158e04b40be0b12e235e5f4c8fec4d430deb8
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-07-19 12:59:12 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "T'Pol", LastName = "Vulcaness" },
+                new PersonModel { FirstName = "Seven", LastName = "Of Nine" },
+                new PersonModel { FirstName = "Nurse", LastName = "Chapel" }
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach (var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{ emp.FirstName } { emp.LastName }: { emp.EmailAddress } IsManager: { emp.IsManager } IsExecutive: { emp.IsExecutive }");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
